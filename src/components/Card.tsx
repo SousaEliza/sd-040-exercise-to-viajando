dd4614ffea375eeea520fa43258a484147f11c9c
Eliza Sousa <eli.sinais@gmail.com>
2024-04-22 15:36:13 -0300

    Estiliza a aplicação

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,11 +7,13 @@ type CardProp = {
 
 function Card({ cityInfo }: { cityInfo: CardProp }) {
   return (
-    <div>
-      <p>{ cityInfo.city }</p>
-      <p>{ cityInfo.country }</p>
+    <div className="city-card">
+      <div id="details">
+        <p id="city-name">{ cityInfo.city }</p>
+        <p id="country-name">{ cityInfo.country }</p>
+        { cityInfo.visited ? <span>Já Fui!</span> : <span>Não Fui.</span>}
+      </div>
       <img src={ cityInfo.imageUrl } alt={ cityInfo.city } />
-      { cityInfo.visited ? <span>Já Fui!</span> : <span>Não Fui.</span>}
     </div>
   );
 }
